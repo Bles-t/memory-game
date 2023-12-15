20c758943fb55ec4dbd51751bed76cdb041f9097
Adrian Bryant <bryant.adrian94@gmail.com>
2023-12-15 16:59:59 -0500

    Added png

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+const cardImages = [
+  { "src": "/img/Array Def.png" },
+  { "src": "/img/Array.png" },
+  { "src": "/img/Conditionals Def.png" },
+  { "src": "/img/Conditionals.png" },
+  { "src": "/img/Function Def.png" },
+  { "src": "/img/Function.png" },
+  { "src": "/img/Loops Def.png" },
+  { "src": "/img/Loops.png" },
+  { "src": "/img/Objects Def.png" },
+  { "src": "/img/Objects.png" },
+  { "src": "/img/Variable Def..png" },
+  { "src": "/img/Variable.png" }
+]
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App;
